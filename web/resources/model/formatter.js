19faf7bfd86ccb7fdcc6bb973e4105c31f44e837
iotjamsession2019 <iotjamsession2019@saschaboll.de>
2019-01-09 17:22:59 +0000

    XSODATA update

--- web/resources/model/formatter.js
+++ web/resources/model/formatter.js
@@ -1,12 +1,8 @@
 sap.ui.define([], function () {
 	"use strict";
 	return {
-		outdoorGetSubHeader: function(sMaxTemp,sMinTemp){
-			return "Max: " + sMaxTemp + " Min: " + sMinTemp + " °C";
-		},
-		getCCUValue : function(sId){
-		  var ts   = (new Date()).getTime(),
-              sUrl = "/api/getBulk/"+sId+"?" + "&ts" + ts;
+		sensorSyncJobStatus : function(sRun,tsLastHeartBeat){
+		  return "test";
 	  }
 	};
 });
